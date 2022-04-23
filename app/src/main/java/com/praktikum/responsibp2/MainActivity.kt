ec3294d2cac2ddfea65c2704a5b89add33d9f712
izhararis <izharcerdas@gmail.com>
2022-04-23 18:08:09 +0700

    Initial commit

---
+++ app/src/main/java/com/praktikum/responsibp2/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.praktikum.responsibp2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnIntent : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnIntent = findViewById(R.id.btn_intent)
+
+        btnIntent.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_intent -> {
+                val intentBiasa = Intent(this@MainActivity,HalamanUtama::class.java)
+                startActivity(intentBiasa)
+            }
+        }
+    }
+}
